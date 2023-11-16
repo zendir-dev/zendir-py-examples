3c75a2b04e6f3c3c42753720c34e7ff8b45252d5
Harrison Verrios <harrison.v1@live.com>
2023-11-16 13:40:24 +1100

    Added in the first set of scenarios available

---
+++ scenarios/scenario_earth_observation.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+'''
+                    [ NOMINAL SYSTEMS ]
+This code is developed by Nominal Systems to aid with communication 
+to the public API. All code is under the the license provided along
+with the 'nominalpy' module. Copyright Nominal Systems, 2023.
+
+TODO EXPLAIN
+'''
+
+# Import the relevant helper scripts
+from nominalpy import Component, Object, Simulation
+from nominalpy import printer, types
+from nominalpy.maths import value, astro
+from credential_helper import *
+import os, time
+from matplotlib import pyplot as plt
+import math
+import matplotlib.image as mpimg
+
+# Clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Set the verbosity
+printer.set_verbosity(printer.SUCCESS_VERBOSITY)
+
+
+############################
+# SIMULATION CONFIGURATION #
+############################
+
+# Construct the credentials
+credentials = fetch_credentials()
+
+# Create a simulation handle
+simulation: Simulation = Simulation(credentials)
+
+# Configure Cesium
+simulation.confiure_cesium("ENTER ACCESS TOKEN")
+
+# Configure the Universe with an epoch
+universe: Object = simulation.get_system(types.UNIVERSE,
+    Epoch=value.datetime(2022, 9, 1))
+
+# Compute the orbit from the Keplerian elements to a state vector of (position, velocity)
+orbit: tuple = astro.classical_to_vector_elements(6671, inclination=35, true_anomaly=16)
+
+# Adds the spacecraft
+spacecraft: Component = simulation.add_component(types.SPACECRAFT,
+    TotalMass=750.0,
+    TotalCenterOfMass=value.vector3(0, 0, 0),
+    TotalMomentOfInertia=value.matrix33((900, 0, 0), (0, 800, 0), (0, 0, 600)),
+    Position=value.vector3(58836722.076589, 269166981.654502, 117536524.015802),
+    Velocity=value.vector3(1428.502224, -264.114737, -1574.090242),
+    AttitudeRate=value.vector3(0.2, 0.1, 0.05))
+
+# Adds a reaction wheel and the stack
+reaction_wheels: Component = simulation.add_component("ReactionWheelArray", spacecraft)
+rw1: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(1, 0, 0))
+rw2: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(0, 1, 0))
+rw3: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(0, 0, 1))
+
+# Adds a simple navigator
+navigator: Component = simulation.add_component("SimpleNavigator", spacecraft)
+
+# Adds in Ephemeris Navigation Converter Software
+eph_convert_fsw: Component = simulation.add_component("EphemerisNavigationConverterSoftware", spacecraft)
+
+# Add in the LVLH pointing software
+lvlh_fsw: Component = simulation.add_component("LVLHPointingSoftware", spacecraft, 
+    In_EphemerisMsg=eph_convert_fsw.get_value("Out_EphemerisMsg"),
+    In_NavTransMsg=navigator.get_value("Out_NavTransMsg"))
+
+# Add an attitude reference correction
+att_reference_fsw: Component = simulation.add_component("AttitudeReferenceCorrectionSoftware", spacecraft,
+    In_AttRefMsg=lvlh_fsw.get_value("Out_AttRefMsg"),
+    Sigma_RcR=value.vector3(0, math.tan(math.pi / 8), 0))
+
+# Add in the attitude tracking error software
+att_tracking_fsw: Component = simulation.add_component("AttitudeTrackingErrorSoftware", spacecraft,
+    In_AttRefMsg=att_reference_fsw.get_value("Out_AttRefMsg"),
+    In_NavAttMsg=navigator.get_value("Out_NavAttMsg"),
+    Sigma_BcB=value.vector3(0, 0, 0))
+
+# Add in the MRP feedback software
+mrp_feedback_fsw: Component = simulation.add_component("MRPFeedbackSoftware", spacecraft,
+    K=3.5,
+    P=30.0,
+    Ki=-1.0,
+    IntegralLimit=-20,
+    In_RWSpeedMsg=reaction_wheels.get_value("Out_RWSpeedMsg"),
+    In_RWArrayConfigMsg=reaction_wheels.get_value("Out_RWArrayConfigMsg"),
+    In_AttGuidMsg=att_tracking_fsw.get_value("Out_AttGuidMsg"),
+    In_VehicleConfigMsg=spacecraft.get_value("Out_VehicleConfigMsg"))
+
+# Add in the motor torque software
+motor_torque_fsw: Component = simulation.add_component("ReactionWheelMotorTorqueSoftware", spacecraft,
+    In_CmdTorqueBodyMsg=mrp_feedback_fsw.get_value("Out_CmdTorqueBodyMsg"),
+    In_RWArrayConfigMsg=reaction_wheels.get_value("Out_RWArrayConfigMsg"))
+
+# Connect up to the reaction wheels
+reaction_wheels.set_value("In_ArrayMotorTorqueMsg", motor_torque_fsw.get_value("Out_ArrayMotorTorqueMsg"))
+
+# Register some messages to be stored in a database
+att_tracking_fsw.get_message("Out_AttGuidMsg").subscribe(5.0)
+reaction_wheels.get_message("Out_RWSpeedMsg").subscribe(5.0)
+
+# Capture two images from the simulation
+simulation.capture_image("images/earth_start_fov50.png", spacecraft, exposure=0.0, fov=50.0, timeout=3.0, 
+    size=(256, 256))
+simulation.capture_image("images/earth_start_fov120.png", spacecraft, exposure=0.0, fov=120.0, timeout=3.0, 
+    size=(256, 256))
+
+# Execute the simulation to be ticked
+simulation.tick(0.05, 5000)
+
+# Capture two final images from the simulation
+simulation.capture_image("images/earth_end_fov5.png", spacecraft, exposure=-0.5, fov=5.0, timeout=3.0, 
+    size=(256, 256), cesium=True)
+simulation.capture_image("images/earth_end_fov1.png", spacecraft, exposure=-0.5, fov=1.0, timeout=3.0, 
+    size=(256, 256), cesium=True)
+
+
+##############################
+# DATA ANALYSIS AND PLOTTING #
+##############################
+
+# Set up the graphs for the data
+figure, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(12, 6))
+
+# Plot the first set of data
+data = att_tracking_fsw.get_message("Out_AttGuidMsg").fetch("Sigma_BR")
+time = value.get_array(data, "time")
+x = value.get_array(data, "Sigma_BR", "X")
+y = value.get_array(data, "Sigma_BR", "Y")
+z = value.get_array(data, "Sigma_BR", "Z")
+ax1.plot(time, x, label = "X [MRP]", color = "red")
+ax1.plot(time, y, label = "Y [MRP]", color = "green")
+ax1.plot(time, z, label = "Z [MRP]", color = "blue")
+
+# Configure the axis
+ax1.set_title("Nadir Pointing Error")
+ax1.set_xlabel("Time [s]")
+ax1.set_ylabel("Sigma [MRP]")
+ax1.legend()
+
+# Plot the third set of data with reaction wheel speeds
+data = reaction_wheels.get_message("Out_RWSpeedMsg").fetch("WheelSpeeds")
+time = value.get_array(data, "time")
+for i in range(3):
+    speeds = value.get_array(data, "WheelSpeeds", index=i)
+    ax4.plot(time, speeds, label = "RW %d Speed [r/s]" % (i + 1), color="cyan")
+
+# Plot the image on the fourth axis
+try:
+    img_start = mpimg.imread('images/earth_start_fov50.png')
+    ax2.imshow(img_start, origin='lower')
+    ax2.set_title("Start Simulation EO Image (FOV: 50 deg)")
+    ax2.set_xlabel("Pixels (X)")
+    ax2.set_ylabel("Pixels (Y)")
+except:
+    printer.error("Unable to find Start image of Earth. Visualiser may not be enabled.")
+
+# Plot the image on the fourth axis
+try:
+    img_start = mpimg.imread('images/earth_start_fov120.png')
+    ax3.imshow(img_start, origin='lower')
+    ax3.set_title("Start Simulation EO Image (FOV: 120 deg)")
+    ax3.set_xlabel("Pixels (X)")
+    ax3.set_ylabel("Pixels (Y)")
+except:
+    printer.error("Unable to find Start image of Earth. Visualiser may not be enabled.")
+
+# Configure the axis
+ax4.set_title("Reaction Wheel Speeds")
+ax4.set_xlabel("Time [s]")
+ax4.set_ylabel("Speeds [r/s]")
+
+# Plot the image on the fifth axis
+try:
+    img_after = mpimg.imread('images/earth_end_fov5.png')
+    ax5.imshow(img_after, origin='lower')
+    ax5.set_title("End Simulation EO Image (FOV: 5 deg)")
+    ax5.set_xlabel("Pixels (X)")
+    ax5.set_ylabel("Pixels (Y)")
+except:
+    printer.error("Unable to find Start image of Earth. Visualiser may not be enabled.")
+
+# Plot the image on the sixth axis
+try:
+    img_after = mpimg.imread('images/earth_end_fov1.png')
+    ax6.imshow(img_after, origin='lower')
+    ax6.set_title("End Simulation EO Image (FOV: 1 deg)")
+    ax6.set_xlabel("Pixels (X)")
+    ax6.set_ylabel("Pixels (Y)")
+except:
+    printer.error("Unable to find Start image of Earth. Visualiser may not be enabled.")
+
+# Show the plots
+plt.tight_layout()
+plt.show()
---
+++ scenarios/scenario_power.py
@@ -0,0 +1,184 @@
+﻿#!/usr/bin/env python3
+
+'''
+                    [ NOMINAL SYSTEMS ]
+This code is developed by Nominal Systems to aid with communication 
+to the public API. All code is under the the license provided along
+with the 'nominalpy' module. Copyright Nominal Systems, 2023.
+
+This example shows a spacecraft with a sun pointing ADCS system that
+will orient the spacecraft to face its solar panel towards the sun,
+using a flight software chain. The solar panel power and pointing
+error is plotted. Additionally, after some time, the spacecraft moves
+to be in eclipse of the Earth, blocking some of the light from the
+sun and preventing the solar panel from producing power.
+'''
+
+# Import the relevant helper scripts
+from nominalpy import Component, Object, Simulation
+from nominalpy import printer, types
+from nominalpy.maths import value, astro
+from credential_helper import *
+import os, time
+from matplotlib import pyplot as plt
+
+# Clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Set the verbosity
+printer.set_verbosity(printer.SUCCESS_VERBOSITY)
+
+
+
+############################
+# SIMULATION CONFIGURATION #
+############################
+
+# Construct the credentials
+credentials = fetch_credentials()
+
+# Create a simulation handle
+simulation: Simulation = Simulation(credentials)
+
+# Configure the Universe with an epoch
+universe: Object = simulation.get_system(types.UNIVERSE,
+    Epoch=value.datetime(2022, 1, 1))
+
+# Compute the orbit from the Keplerian elements to a state vector of (position, velocity)
+orbit: tuple = astro.classical_to_vector_elements(6671, inclination=35, true_anomaly=16)
+
+# Adds the spacecraft
+spacecraft: Component = simulation.add_component(types.SPACECRAFT,
+    TotalMass=750.0,
+    TotalCenterOfMass=value.vector3(0, 0, 0),
+    TotalMomentOfInertia=value.matrix33((900, 0, 0), (0, 800, 0), (0, 0, 600)),
+    Position=orbit[0],
+    Velocity=orbit[1],
+    AttitudeRate=value.vector3(0.2, 0.1, 0.05))
+
+# Adds a reaction wheel and the stack
+reaction_wheels: Component = simulation.add_component("ReactionWheelArray", spacecraft)
+rw1: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(1, 0, 0))
+rw2: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(0, 1, 0))
+rw3: Component = simulation.add_component("ReactionWheel", reaction_wheels, 
+    WheelSpinAxis_B=value.vector3(0, 0, 1))
+
+# Adds a simple navigator
+navigator: Component = simulation.add_component("SimpleNavigator", spacecraft)
+
+# Adds a solar panel
+solar_panel: Component = simulation.add_component("SolarPanel", spacecraft, 
+    Area=0.01, Efficiency=0.23)
+
+# Adds in Sun Safe Pointing
+sun_point_fsw: Component = simulation.add_component("SunSafePointingSoftware", spacecraft,
+    MinUnitMag=0.001,
+    SmallAngle=0.001,
+    SunBodyVector=solar_panel.get_value("LocalUp"),
+    Omega_RN_B=value.vector3(0, 0, 0),
+    SunAxisSpinRate=0.0,
+    In_NavAttMsg=navigator.get_value("Out_NavAttMsg"),
+    In_SunDirectionMsg=navigator.get_value("Out_NavAttMsg"))
+
+# Add in the MRP feedback software
+mrp_feedback_fsw: Component = simulation.add_component("MRPFeedbackSoftware", spacecraft,
+    K=3.5,
+    P=30.0,
+    Ki=-1.0,
+    IntegralLimit=-20,
+    In_RWSpeedMsg=reaction_wheels.get_value("Out_RWSpeedMsg"),
+    In_RWArrayConfigMsg=reaction_wheels.get_value("Out_RWArrayConfigMsg"),
+    In_AttGuidMsg=sun_point_fsw.get_value("Out_AttGuidMsg"),
+    In_VehicleConfigMsg=spacecraft.get_value("Out_VehicleConfigMsg"))
+
+# Add in the motor torque software
+motor_torque_fsw: Component = simulation.add_component("ReactionWheelMotorTorqueSoftware", spacecraft,
+    In_CmdTorqueBodyMsg=mrp_feedback_fsw.get_value("Out_CmdTorqueBodyMsg"),
+    In_RWArrayConfigMsg=reaction_wheels.get_value("Out_RWArrayConfigMsg"))
+
+# Connect up to the reaction wheels
+reaction_wheels.set_value("In_ArrayMotorTorqueMsg", motor_torque_fsw.get_value("Out_ArrayMotorTorqueMsg"))
+
+# Register some messages to be stored in a database
+spacecraft.get_message("Out_EclipseMsg").subscribe(5.0)
+navigator.get_message("Out_NavAttMsg").subscribe(5.0)
+sun_point_fsw.get_message("Out_AttGuidMsg").subscribe(5.0)
+solar_panel.get_message("Out_PowerSourceMsg").subscribe(5.0)
+reaction_wheels.get_message("Out_RWSpeedMsg").subscribe(5.0)
+
+# Execute the simulation to be ticked
+simulation.tick(0.05, 2000)
+
+
+
+##############################
+# DATA ANALYSIS AND PLOTTING #
+##############################
+
+# Set up the graphs for the data
+figure, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 6))
+
+# Plot the first set of data
+data = sun_point_fsw.get_message("Out_AttGuidMsg").fetch("Sigma_BR")
+time = value.get_array(data, "time")
+x = value.get_array(data, "Sigma_BR", "X")
+y = value.get_array(data, "Sigma_BR", "Y")
+z = value.get_array(data, "Sigma_BR", "Z")
+ax1.plot(time, x, label = "X [MRP]", color = "red")
+ax1.plot(time, y, label = "Y [MRP]", color = "green")
+ax1.plot(time, z, label = "Z [MRP]", color = "blue")
+
+# Configure the axis
+ax1.set_title("Sun Pointing Error")
+ax1.set_xlabel("Time [s]")
+ax1.set_ylabel("Sigma [MRP]")
+ax1.legend()
+
+# Plot the second set of data of current attitude
+data = navigator.get_message("Out_NavAttMsg").fetch("Sigma_BN")
+time = value.get_array(data, "time")
+x = value.get_array(data, "Sigma_BN", "X")
+y = value.get_array(data, "Sigma_BN", "Y")
+z = value.get_array(data, "Sigma_BN", "Z")
+ax2.plot(time, x, label = "X [MRP]", color = "red")
+ax2.plot(time, y, label = "Y [MRP]", color = "green")
+ax2.plot(time, z, label = "Z [MRP]", color = "blue")
+
+# Configure the axis
+ax2.set_title("Sun Pointing Attitude")
+ax2.set_xlabel("Time [s]")
+ax2.set_ylabel("Attitude [MRP]")
+ax2.legend()
+
+# Plot the third set of data with power and visibility
+data = solar_panel.get_message("Out_PowerSourceMsg").fetch("Power")
+time = value.get_array(data, "time")
+power = value.get_array(data, "Power")
+data = spacecraft.get_message("Out_EclipseMsg").fetch("Visibility")
+visibility = value.get_array(data, "Visibility")
+ax3.plot(time, power, label = "Power [W]", color = "orange")
+ax3.plot(time, visibility, label = "Sun Visibility", color = "pink")
+
+# Configure the axis
+ax3.set_title("Solar Panel Power")
+ax3.set_xlabel("Time [s]")
+ax3.set_ylabel("Incident Power [W]")
+ax3.legend()
+
+# Plot the fourth set of data with reaction wheel speeds
+data = reaction_wheels.get_message("Out_RWSpeedMsg").fetch("WheelSpeeds")
+time = value.get_array(data, "time")
+for i in range(3):
+    speeds = value.get_array(data, "WheelSpeeds", index=i)
+    ax4.plot(time, speeds, label = "RW %d Speed [r/s]" % (i + 1), color="cyan")
+
+# Configure the axis
+ax4.set_title("Reaction Wheel Speeds")
+ax4.set_xlabel("Time [s]")
+ax4.set_ylabel("Speeds [r/s]")
+
+# Show the plots
+plt.tight_layout()
+plt.show()
