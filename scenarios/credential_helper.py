931c4a10f2fe53b2dfbd2c87ae4ba51213811d07
Sam Huang <sam@zendir.io>
2025-07-08 17:01:33 +1000

    Updated examples to work with new Client

--- scenarios/credential_helper.py
+++ scenarios/credential_helper.py
@@ -1,32 +1,43 @@
-"""
+'''
                     [ NOMINAL SYSTEMS ]
-This code is developed by Nominal Systems to aid with communication
+This code is developed by Nominal Systems to aid with communication 
 to the public API. All code is under the the license provided along
 with the 'nominalpy' module. Copyright Nominal Systems, 2024.
 
 This method allows for easy exporting of custom-defined credentials,
 specified within the parameters below. Importing this method into
 scenario files enables easy access to the correct credentials of
-the system.
-"""
+the system. When using the public API, make sure to enter your access
+key to the GLOBAL_KEY parameter.
+'''
 
 from nominalpy import Client
 
-# Defines the credentials
-LOCAL_URL: str = "http://127.0.0.1:25565"
-GLOBAL_URL: str = "https://api.zendir.io"
-GLOBAL_KEY: str = ""
+# Defines the credentials for a Local API connection
+LOCAL_URL: str = "http://localhost"
+LOCAL_PORT: int = 25565
+LOCAL_KEY: str = ""
+
+# Defines the credentials for a Public API connection
+GLOBAL_URL: str = "https://api.nominalsys.com"
+GLOBAL_PORT: int = None
+GLOBAL_KEY: str = ""        # This is where the access key for the API should be added
 
 # The flag that sets whether to use the public API or the local one.
 USE_PUBLIC_API: bool = False
 
 
-def fetch_client() -> Client:
-    """
-    Construct a sample client that can be used for all files that are
-    passing in credentials to the simulation. This works for both the
-    public and local API, provided that the flag USE_PUBLIC_API is called.
-    """
+def fetch_client () -> Client:
+    '''
+    Constructs some sample credentials that can be used for 
+    all files that are passing in credentials to the simulation.
+    This works for both the public and local API, provided that
+    the flag USE_PUBLIC_API is called.
+
+    :returns:   The credentials for the API connection
+    :rtype:     Credentials
+    '''
+    
     if USE_PUBLIC_API:
-        return Client(GLOBAL_URL, token=GLOBAL_KEY)
-    return Client(LOCAL_URL)
+        return Client(url=GLOBAL_URL, token=GLOBAL_KEY)
+    return Client.create_local(LOCAL_PORT)
