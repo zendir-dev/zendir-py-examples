53a7a99e30a0bd41f4239d2a07173fe6ee8cc886
Harrison Verrios <harrison@zendir.io>
2025-07-09 14:24:48 +1000

    Updated the default so that the 'zendir.io' and it by default points to the public API instead of local (for the public release).

--- scenarios/credential_helper.py
+++ scenarios/credential_helper.py
@@ -1,6 +1,6 @@
-'''
+"""
                     [ NOMINAL SYSTEMS ]
-This code is developed by Nominal Systems to aid with communication 
+This code is developed by Nominal Systems to aid with communication
 to the public API. All code is under the the license provided along
 with the 'nominalpy' module. Copyright Nominal Systems, 2024.
 
@@ -9,35 +9,32 @@ specified within the parameters below. Importing this method into
 scenario files enables easy access to the correct credentials of
 the system. When using the public API, make sure to enter your access
 key to the GLOBAL_KEY parameter.
-'''
+"""
 
 from nominalpy import Client
 
 # Defines the credentials for a Local API connection
-LOCAL_URL: str = "http://localhost"
 LOCAL_PORT: int = 25565
-LOCAL_KEY: str = ""
 
 # Defines the credentials for a Public API connection
-GLOBAL_URL: str = "https://api.nominalsys.com"
-GLOBAL_PORT: int = None
-GLOBAL_KEY: str = ""        # This is where the access key for the API should be added
+GLOBAL_URL: str = "https://api.zendir.io"
+API_TOKEN: str = ""  # This is where the access key for the API should be added
 
 # The flag that sets whether to use the public API or the local one.
-USE_PUBLIC_API: bool = False
+USE_PUBLIC_API: bool = True
 
 
-def fetch_client () -> Client:
-    '''
-    Constructs some sample credentials that can be used for 
+def fetch_client() -> Client:
+    """
+    Constructs some sample credentials that can be used for
     all files that are passing in credentials to the simulation.
     This works for both the public and local API, provided that
     the flag USE_PUBLIC_API is called.
 
     :returns:   The credentials for the API connection
     :rtype:     Credentials
-    '''
-    
+    """
+
     if USE_PUBLIC_API:
-        return Client(url=GLOBAL_URL, token=GLOBAL_KEY)
+        return Client(url=GLOBAL_URL, token=API_TOKEN)
     return Client.create_local(LOCAL_PORT)
